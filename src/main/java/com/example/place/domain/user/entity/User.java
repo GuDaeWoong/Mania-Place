97659b0258bf8fc84657f4acf85b64b7b0fb435a
GuDaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-07-17 12:51:05 +0900

    feat: 도메인 엔티티 생성

---
+++ src/main/java/com/example/place/domain/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.place.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String nickname;
+	private String email;
+	private String password;
+	// private String tags;
+	@Enumerated(EnumType.STRING)
+	private UserRole role;
+}
