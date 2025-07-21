d1c953024f5c5045cf25de4c8a21377a9f0af3aa
Che0807 <elpin0428@naver.com>
2025-07-21 12:14:12 +0900

    feat: UserRepository findByEmail 메서드 추가

--- src/main/java/com/example/place/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/place/domain/user/repository/UserRepository.java
@@ -1,8 +1,14 @@
 package com.example.place.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.place.domain.user.entity.User;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(@Email @NotBlank String email);
 }
