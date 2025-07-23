57c8a86c7ae66713e0a2ef36f786d690bfea7da7
Che0807 <elpin0428@naver.com>
2025-07-23 17:22:25 +0900

    feat: LoginResponseDto refreshToken 필드 추가

--- src/main/java/com/example/place/domain/auth/controller/dto/LoginResponseDto.java
+++ src/main/java/com/example/place/domain/auth/controller/dto/LoginResponseDto.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 @Getter
 public class LoginResponseDto {
 	private final String accessToken;
+	private final String refreshToken;
 
-	public LoginResponseDto(String accessToken) {
+	public LoginResponseDto(String accessToken,String refreshToken) {
 		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
 	}
 }
