f6d21379b5cf264ed862307e474aa5a10813b06b
GuDaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-07-24 12:07:03 +0900

    refactor : PageResponseDto 적용 및 N+1 해결

--- src/main/java/com/example/place/domain/postcomment/service/PostCommnetService.java
+++ src/main/java/com/example/place/domain/postcomment/service/PostCommnetService.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.example.place.common.dto.PageResponseDto;
 import com.example.place.common.exception.enums.ExceptionCode;
 import com.example.place.common.exception.exceptionclass.CustomException;
 import com.example.place.common.security.jwt.CustomPrincipal;
@@ -68,17 +69,18 @@ public class PostCommnetService {
 		);
 	}
 
-	public Page<PostCommentResponse> getCommentsByPost(Long postId, Pageable pageable) {
+	public PageResponseDto<PostCommentResponse> getCommentsByPost(Long postId, Pageable pageable
+	){
 		Post post = postService.findByIdOrElseThrow(postId);
 
 		Page<PostComment> commentPage = postCommentRepository.findAllByPost(post, pageable);
 
-		return commentPage.map(comment -> new PostCommentResponse(
+		Page<PostCommentResponse> responsePage = commentPage.map(comment -> new PostCommentResponse(
 			comment.getUser().getNickname(),
 			comment.getUser().getImageUrl(),
 			comment.getContent(),
 			comment.getCreatedAt()
 		));
-
+		return new PageResponseDto<>(responsePage);
 	}
 }
