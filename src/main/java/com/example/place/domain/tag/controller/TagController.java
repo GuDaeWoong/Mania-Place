35c51de343a2276c6a732b2464889e42e44d5ae2
GuDaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-07-25 14:27:40 +0900

    refactor: deleteTag 인증 정보 삭제 - @AuthenticationPrincipal CustomPrincipal principal

--- src/main/java/com/example/place/domain/tag/controller/TagController.java
+++ src/main/java/com/example/place/domain/tag/controller/TagController.java
@@ -1,7 +1,6 @@
 package com.example.place.domain.tag.controller;
 
 import com.example.place.common.dto.ApiResponseDto;
-import com.example.place.common.security.jwt.CustomPrincipal;
 import com.example.place.domain.tag.dto.request.TagRequest;
 import com.example.place.domain.tag.dto.response.TagResponse;
 import com.example.place.domain.tag.service.TagService;
@@ -9,7 +8,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -41,8 +39,7 @@ public class TagController {
 
     @DeleteMapping("/{tagId}")
     public ResponseEntity<ApiResponseDto<Void>> deleteTag(
-            @PathVariable Long tagId,
-            @AuthenticationPrincipal CustomPrincipal principal
+            @PathVariable Long tagId
     ) {
         tagService.deleteTag(tagId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponseDto.of("태그가 삭제되었습니다.",null));
