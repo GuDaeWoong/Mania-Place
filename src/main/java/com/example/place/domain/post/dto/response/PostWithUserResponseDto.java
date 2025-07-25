90ed1818ab03341ea2cec0b71305fd27e248bef4
saevit <woosaevit@gmail.com>
2025-07-25 11:10:13 +0900

    feat: 게시글 응답DTO에 이미지url목록과 대표 인덱스 추가

--- src/main/java/com/example/place/domain/post/dto/response/PostWithUserResponseDto.java
+++ src/main/java/com/example/place/domain/post/dto/response/PostWithUserResponseDto.java
@@ -1,5 +1,9 @@
 package com.example.place.domain.post.dto.response;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.place.domain.Image.entity.Image;
 import com.example.place.domain.post.entity.Post;
 
 import lombok.Getter;
@@ -10,11 +14,25 @@ public class PostWithUserResponseDto {
 	private final String content;
 	private final String nickname;
 	private final Long itemId;
+	private List<String> imageUrls;
+	private int mainIndex;
 
 	public PostWithUserResponseDto(Post post,String nickname) {
+		List<Image> images = post.getItem().getImages();
+		List<String> imageUrls = new ArrayList<>();
+		int mainIndex = 0;
+		for (int i = 0; i < images.size(); i++) {
+			imageUrls.add(images.get(i).getImageUrl());
+			if (images.get(i).isMain()) {
+				mainIndex = i;
+			}
+		}
+
 		this.id = post.getId();
 		this.content = post.getContent();
-		this.itemId = post.getItem().getId();
 		this.nickname = nickname;
+		this.itemId = post.getItem().getId();
+		this.imageUrls = imageUrls;
+		this.mainIndex = mainIndex;
 	}
 }
