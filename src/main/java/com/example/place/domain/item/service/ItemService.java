f6703991a487c5ea40bcc410329ff5e38824655e
GuDaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-07-30 10:46:03 +0900

    feature: 상품 생성 한정상품 설정기능 추가 userRole이 admin 인 경우 자동적으로 true로 입력 userRole이 user 인 경우 자동적으로 false 입력

--- src/main/java/com/example/place/domain/item/service/ItemService.java
+++ src/main/java/com/example/place/domain/item/service/ItemService.java
@@ -10,6 +10,7 @@ import com.example.place.domain.item.repository.ItemRepository;
 
 import com.example.place.domain.tag.service.TagService;
 import com.example.place.domain.user.entity.User;
+import com.example.place.domain.user.entity.UserRole;
 import com.example.place.domain.user.service.UserService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -26,7 +27,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
 
 @Service
 @RequiredArgsConstructor
@@ -59,12 +59,16 @@ public class ItemService {
         User user = userService.findByIdOrElseThrow(userId);
 		List<LocalDateTime> sales_start0_end1 = setSalesTime(request);
 
+		boolean isAdminItem = user.getRole() == UserRole.ADMIN;
+
         Item item = Item.of(
                 user,
 				request.getItemName(),
 				request.getItemDescription(),
 				request.getPrice(),
-				request.getCount(),
+				request.getTotalCount(),
+				request.getTotalCount(),
+				isAdminItem,
 				sales_start0_end1.get(0),
 				sales_start0_end1.get(1)
 				);
