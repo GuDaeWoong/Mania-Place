70a99b3f681f9825863caa6556ca36733732647a
hohohosn <day7of01@gmail.com>
2025-08-01 04:37:57 +0900

    fix(newsfeed): 전체 조회 +작성일,수정일

--- src/main/java/com/example/place/domain/newsfeed/dto/response/NewsfeedListResponse.java
+++ src/main/java/com/example/place/domain/newsfeed/dto/response/NewsfeedListResponse.java
@@ -1,5 +1,7 @@
 package com.example.place.domain.newsfeed.dto.response;
 
+import java.time.LocalDateTime;
+
 import com.example.place.domain.newsfeed.entity.Newsfeed;
 
 import lombok.AccessLevel;
@@ -12,18 +14,25 @@ public class NewsfeedListResponse {
 	private Long id;
 	private String title;
 	private String mainImageUrl;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 
-	private NewsfeedListResponse(Long id, String title, String mainImageUrl) {
+	private NewsfeedListResponse(Long id, String title, String mainImageUrl, LocalDateTime createdAt,
+		LocalDateTime updatedAt) {
 		this.id = id;
 		this.title = title;
 		this.mainImageUrl = mainImageUrl;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
 	}
 
 	public static NewsfeedListResponse of(Newsfeed newsfeed, String mainImageUrl) {
 		return new NewsfeedListResponse(
 			newsfeed.getId(),
 			newsfeed.getTitle(),
-			mainImageUrl
+			mainImageUrl,
+			newsfeed.getCreatedAt(),
+			newsfeed.getUpdatedAt()
 		);
 	}
 }
