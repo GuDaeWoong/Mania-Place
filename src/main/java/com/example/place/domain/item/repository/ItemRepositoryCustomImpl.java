7ed1d2e49d9ab7d2731382b7c644f54f8a76095f
saevit <woosaevit@gmail.com>
2025-07-31 12:21:14 +0900

    refactor: ItemRepository Query DSL 공통 부분 별도의 메서드로 분리

--- src/main/java/com/example/place/domain/item/repository/ItemRepositoryCustomImpl.java
+++ src/main/java/com/example/place/domain/item/repository/ItemRepositoryCustomImpl.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 
 import com.example.place.domain.item.entity.Item;
 import com.example.place.domain.user.entity.User;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -29,57 +30,47 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
 	@Override
 	public Page<Item> findByUserTag(User user, Pageable pageable) {
 
-		// 페이지의 id 조회
-		// --유저의 관심 태그를 조회하는 서브쿼리
+		// 유저의 관심 태그를 조회하는 서브쿼리
 		JPQLQuery<Long> interestedTagIds = JPAExpressions
 			.select(userTag.tag.id)
 			.from(userTag)
 			.where(userTag.user.eq(user));
-		// --유저의 관심 태그가 있는 상품 id 조회
-		List<Long> itemIds = queryFactory
-			.select(item.id)
-			.from(item)
-			.join(item.itemTags, itemTag)
-			.join(itemTag.tag, tag)
-			.where(
-				item.isDeleted.isFalse(),
-				tag.id.in(interestedTagIds))
-			.offset(pageable.getOffset())
-			.limit(pageable.getPageSize())
-			.fetch();
+		// 유저의 관심 태그가 있는 상품 조회를 위한 where절
+		BooleanExpression whereCondition = tag.id.in(interestedTagIds);
 
-		if (itemIds.isEmpty()) {
-			return new PageImpl<>(Collections.emptyList(), pageable, 0);
-		}
-
-		// 페이지의 상품들 정보 조회
-		List<Item> items = queryFactory
-			.selectFrom(item)
-			.distinct()
-			.join(item.itemTags, itemTag).fetchJoin()
-			.join(itemTag.tag, tag).fetchJoin()
-			.join(item.images).fetchJoin()
-			.where(item.id.in(itemIds))
-			.fetch();
-
-		// 페이징 처리를 위한 전체 열 카운트
-		long total = Optional.ofNullable(
-			queryFactory
-				.select(item.countDistinct()) // fetchJoin 없이 count 하기위해 distinct
-				.from(item)
-				.join(item.itemTags, itemTag)
-				.join(itemTag.tag, tag)
-				.where(item.isDeleted.isFalse(),
-					tag.id.in(interestedTagIds))
-				.fetchOne()
-		).orElse(0L);
-
-		return new PageImpl<>(items, pageable, total);
+		return createPagedItem(whereCondition, pageable);
 	}
 
 	@Override
 	public Page<Item> search(String keyword, List<String> tags, Long userId, Pageable pageable) {
 
+		// 상품 조회를 위한 where절
+		BooleanExpression whereCondition = null;
+
+		// 키워드를 전달 받았다면 해당 키워드가 제목or설명에 포함된 상품을 조회
+		if (keyword != null) {
+			BooleanExpression keywordCondition = item.itemName.likeIgnoreCase("%" + keyword + "%")
+				.or(item.itemDescription.likeIgnoreCase("%" + keyword + "%"));
+			whereCondition = whereCondition == null ? keywordCondition : whereCondition.and(keywordCondition);
+		}
+
+		// 태그를 전달 받았다면 해당 태그가 존재하는 상품을 조회
+		if (tags != null && !tags.isEmpty()) {
+			BooleanExpression tagCondition = tag.tagName.in(tags);
+			whereCondition = whereCondition == null ? tagCondition : whereCondition.and(tagCondition);
+		}
+
+		// 유저 id를 전달 받았다면 해당 판매자가 해당 id와 일치하는 상품을 조회
+		if (userId != null) {
+			BooleanExpression userCondition = item.user.id.eq(userId);
+			whereCondition = whereCondition == null ? userCondition : whereCondition.and(userCondition);
+		}
+
+		return createPagedItem(whereCondition, pageable);
+	}
+
+	private Page<Item> createPagedItem(BooleanExpression whereCondition, Pageable pageable) {
+
 		// 페이지의 id 조회
 		List<Long> itemIds = queryFactory
 			.select(item.id)
@@ -88,10 +79,7 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
 			.join(itemTag.tag, tag)
 			.where(
 				item.isDeleted.isFalse(),
-				keyword != null ? (item.itemName.likeIgnoreCase("%" + keyword + "%"))
-					.or(item.itemDescription.likeIgnoreCase("%" + keyword + "%")) : null,
-				(tags != null && !tags.isEmpty()) ? tag.tagName.in(tags) : null,
-				userId != null ? item.user.id.eq(userId) : null)
+				whereCondition)
 			.offset(pageable.getOffset())
 			.limit(pageable.getPageSize())
 			.fetch();
@@ -119,10 +107,7 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom {
 				.join(itemTag.tag, tag)
 				.where(
 					item.isDeleted.isFalse(),
-					keyword != null ? (item.itemName.likeIgnoreCase("%" + keyword + "%"))
-						.or(item.itemDescription.likeIgnoreCase("%" + keyword + "%")) : null,
-					(tags != null && !tags.isEmpty()) ? tag.tagName.in(tags) : null,
-					userId != null ? item.user.id.eq(userId) : null)
+					whereCondition)
 				.fetchOne()
 		).orElse(0L);
 
