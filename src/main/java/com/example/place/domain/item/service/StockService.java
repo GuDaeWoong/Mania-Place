b847f78a3ecbefd594514a4b3a2f8d460483a218
GuDaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-08-01 16:49:32 +0900

    feature : 한정 상품인 경우 비관락 구현 - 한정 상품이냐 아니냐를 기준으로 분산락 비관락 방법 분류

--- src/main/java/com/example/place/domain/item/service/StockService.java
+++ src/main/java/com/example/place/domain/item/service/StockService.java
@@ -22,28 +22,29 @@ public class StockService {
 	private final RedissonClient redissonClient;
 
 	// 재고 감소 로직
-	@Transactional
 	public void decreaseStock(Long itemId, Long quantity) {
 		Item item = itemService.findByIdOrElseThrow(itemId);
-		// 총 개수 1개 + 일반 상품
-		if (item.getCount() == 1L) {
+		if (!item.isLimitedEdition()) {
 			decreaseStockWithDistributedLock(itemId, quantity);
+		} else {
+			decreaseStockWithRedisPessimisticLock(itemId, quantity);
 		}
 	}
 
 	// 재고 증가 로직
-	@Transactional
 	public void increaseStock(Long itemId, Long quantity) {
 		Item item = itemService.findByIdOrElseThrow(itemId);
 
 		if (item.getCount() == 1L && !item.isLimitedEdition()) {
 			increaseStockWithDistributedLock(itemId, quantity);
+		} else {
+			increaseStockWithRedisPessimisticLock(itemId, quantity);
 		}
 	}
 
 	// 분산락 재고감소
 	private void decreaseStockWithDistributedLock(Long itemId, Long quantity) {
-		String lockKey = "lock:" + itemId;
+		String lockKey = "DistributedLock:" + itemId;
 		RLock lock = redissonClient.getLock(lockKey);
 
 		try {
@@ -62,7 +63,7 @@ public class StockService {
 			}
 			// 실제 제고감소
 			item.decreaseStock(quantity);
-		}catch (InterruptedException e) {
+		} catch (InterruptedException e) {
 			Thread.currentThread().interrupt();
 			throw new CustomException(ExceptionCode.OPERATION_INTERRUPTED);
 		} finally {
@@ -74,7 +75,7 @@ public class StockService {
 
 	// 분산락 재고증가
 	private void increaseStockWithDistributedLock(Long itemId, Long quantity) {
-		String lockKey = "lock:" + itemId;
+		String lockKey = "DistributedLock:" + itemId;
 		RLock lock = redissonClient.getLock(lockKey);
 		try {
 			boolean acquired = lock.tryLock(3000, 10000, TimeUnit.MILLISECONDS);
@@ -84,6 +85,58 @@ public class StockService {
 
 			Item item = itemService.findByIdOrElseThrow(itemId);
 			// 재고 증가
+			item.increaseStock(quantity);
+
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new CustomException(ExceptionCode.OPERATION_INTERRUPTED);
+		} finally {
+			if (lock.isHeldByCurrentThread()) {
+				lock.unlock();
+			}
+		}
+	}
+
+	// Redis 비관적 락
+	private void decreaseStockWithRedisPessimisticLock(Long itemId, Long quantity) {
+		String lockKey = "PessimisticLock:" + itemId;
+		RLock lock = redissonClient.getLock(lockKey);
+		try {
+			boolean acquired = lock.tryLock(3000, 10000, TimeUnit.MILLISECONDS);
+
+			if (!acquired) {
+				throw new CustomException(ExceptionCode.STOCK_LOCK_FAILED);
+			}
+
+			Item item = itemService.findByIdOrElseThrow(itemId);
+
+			if (item.getCount() < quantity) {
+				throw new CustomException(ExceptionCode.OUT_OF_STOCK);
+			}
+
+			item.decreaseStock(quantity);
+
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new CustomException(ExceptionCode.OPERATION_INTERRUPTED);
+		} finally {
+			if (lock.isHeldByCurrentThread()) {
+				lock.unlock();
+			}
+		}
+	}
+
+	private void increaseStockWithRedisPessimisticLock(Long itemId, Long quantity) {
+		String lockKey = "PessimisticLock:" + itemId;
+		RLock lock = redissonClient.getLock(lockKey);
+		try {
+			boolean acquired = lock.tryLock(3000, 10000, TimeUnit.MILLISECONDS);
+			if (!acquired) {
+				throw new CustomException(ExceptionCode.STOCK_LOCK_FAILED);
+			}
+
+			Item item = itemService.findByIdOrElseThrow(itemId);
+
 			item.increaseStock(quantity);
 
 		} catch (InterruptedException e) {
