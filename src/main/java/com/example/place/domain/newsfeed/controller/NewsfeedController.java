f8871268c665c0fab3f77b848f6303f07554b661
hohohosn <day7of01@gmail.com>
2025-08-05 05:12:13 +0900

    feature(newsfeed): admin 권한 확인

--- src/main/java/com/example/place/domain/newsfeed/controller/NewsfeedController.java
+++ src/main/java/com/example/place/domain/newsfeed/controller/NewsfeedController.java
@@ -2,8 +2,6 @@ package com.example.place.domain.newsfeed.controller;
 
 import com.example.place.common.dto.ApiResponseDto;
 import com.example.place.common.dto.PageResponseDto;
-import com.example.place.common.exception.enums.ExceptionCode;
-import com.example.place.common.exception.exceptionclass.CustomException;
 import com.example.place.common.security.jwt.CustomPrincipal;
 import com.example.place.domain.newsfeed.dto.request.NewsfeedRequest;
 import com.example.place.domain.newsfeed.dto.response.NewsfeedListResponse;
@@ -14,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,12 +28,11 @@ public class NewsfeedController {
 
 	//새소식 생성
 	@PostMapping
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<ApiResponseDto<NewsfeedResponse>> createNewsfeed(
 		@Valid @RequestBody NewsfeedRequest request,
 		@AuthenticationPrincipal CustomPrincipal principal
 	) {
-		// Admin 권한 확인
-		checkAdminRole(principal);
 
 		NewsfeedResponse newsfeed = newsfeedService.createNewsfeed(principal.getId(), request);
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponseDto.of("게시글 등록이 완료되었습니다.", newsfeed));
@@ -42,6 +40,7 @@ public class NewsfeedController {
 
 	//단건 조회
 	@GetMapping("/{newsfeedId}")
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 	public ResponseEntity<ApiResponseDto<NewsfeedResponse>> getNewsfeed(@PathVariable Long newsfeedId) {
 		NewsfeedResponse newsfeed = newsfeedService.getNewsfeed(newsfeedId);
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponseDto.of("게시글 조회가 완료되었습니다.", newsfeed));
@@ -49,6 +48,7 @@ public class NewsfeedController {
 
 	//전체 조회
 	@GetMapping
+	@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
 	public ResponseEntity<ApiResponseDto<PageResponseDto<NewsfeedListResponse>>> getAllNewsfeeds(
 		@PageableDefault Pageable pageable
 	) {
@@ -60,14 +60,12 @@ public class NewsfeedController {
 
 	//새소식 수정
 	@PatchMapping("/{newsfeedId}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<ApiResponseDto<NewsfeedResponse>> updateNewsfeed(
 		@PathVariable Long newsfeedId,
 		@RequestBody NewsfeedRequest request,
 		@AuthenticationPrincipal CustomPrincipal principal
 	) {
-
-		// Admin 권한 확인
-		checkAdminRole(principal);
 
 		NewsfeedResponse updateNewsfeed = newsfeedService.updateNewsfeed(newsfeedId, request, principal.getId());
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponseDto.of("게시글 수정이 완료되었습니다.", updateNewsfeed));
@@ -75,27 +73,13 @@ public class NewsfeedController {
 
 	//새소식 삭제
 	@DeleteMapping("/{newsfeedId}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<ApiResponseDto<Void>> softDeleteNewsfeed(
 		@PathVariable Long newsfeedId,
 		@AuthenticationPrincipal CustomPrincipal principal
 	) {
 
-		// Admin 권한 확인
-		checkAdminRole(principal);
-
 		newsfeedService.softDeleteNewsfeed(newsfeedId, principal.getId());
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponseDto.of("게시글 삭제가 완료되었습니다.", null));
-	}
-
-	// Admin 권한 확인
-	private void checkAdminRole(CustomPrincipal principal) {
-		// authorities를 사용
-		boolean isAdmin = principal.getAuthorities().stream()
-			.anyMatch(auth -> "ROLE_ADMIN".equals(auth.getAuthority()) ||
-				"ADMIN".equals(auth.getAuthority()));
-
-		if (!isAdmin) {
-			throw new CustomException(ExceptionCode.FORBIDDEN_ADMIN_ACCESS);
-		}
 	}
 }
