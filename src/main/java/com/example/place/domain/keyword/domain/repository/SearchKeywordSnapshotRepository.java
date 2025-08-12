fe74f38cacdbd12ec7635658782a96682931fea5
Che0807 <elpin0428@naver.com>
2025-08-12 19:51:56 +0900

    refactor: 미사용 쿼리 제거

--- src/main/java/com/example/place/domain/keyword/domain/repository/SearchKeywordSnapshotRepository.java
+++ src/main/java/com/example/place/domain/keyword/domain/repository/SearchKeywordSnapshotRepository.java
@@ -13,11 +13,6 @@ import com.example.place.domain.keyword.domain.model.KeywordSnapshot;
 
 public interface SearchKeywordSnapshotRepository extends JpaRepository<KeywordSnapshot, Long> {
 
-	// 최근 스냅샷 가져오기
-	@Query("SELECT ks FROM KeywordSnapshot ks WHERE ks.snapshotTime = " +
-		"(SELECT MAX(ks2.snapshotTime) FROM KeywordSnapshot ks2 WHERE ks2.snapshotTime <= :time)")
-	List<KeywordSnapshot> findLatestSnapshotsBefore(@Param("time") LocalDateTime time);
-
 	// 데이터 정리
 	@Modifying
 	@Transactional
