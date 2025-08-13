b162d689f0c5b2162f2a09c332f7e0b1fe8b08f5
윤희준 <helloowbeter@naver.com>
2025-08-13 12:45:07 +0900

    feature/RabbitBQ

--- src/main/java/com/example/place/domain/chatmessage/controller/ChatMessageController.java
+++ src/main/java/com/example/place/domain/chatmessage/controller/ChatMessageController.java
@@ -4,19 +4,18 @@ import com.example.place.common.exception.enums.ExceptionCode;
 import com.example.place.common.exception.exceptionclass.CustomException;
 import com.example.place.common.security.jwt.CustomPrincipal;
 import com.example.place.domain.chatmessage.dto.request.ChatMessageRequest;
-import com.example.place.domain.chatmessage.service.ChatMessageService;
+import com.example.place.domain.chatmessage.entity.MessageQueue;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
-import java.security.Principal;
+import java.time.LocalDateTime;
 
 
 @Slf4j
@@ -24,26 +23,21 @@ import java.security.Principal;
 @RequiredArgsConstructor
 public class ChatMessageController {
 
-    private final ChatMessageService chatMessageService;
     private final SimpMessagingTemplate messagingTemplate;
+    private final RabbitTemplate rabbitTemplate;
+    private static final String CHAT_QUEUE_NAME = "chat.queue";
 
     @MessageMapping("/chatroom/{roomId}")
     public void sendMessage(
             @DestinationVariable Long roomId,
             @Valid @Payload ChatMessageRequest request,
-//            Message<?> message,
             CustomPrincipal principal
     ) {
-//        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-//        CustomPrincipal principal = (CustomPrincipal) accessor.getSessionAttributes().get("user");
-//        CustomPrincipal principal = (CustomPrincipal) principal;
         if (principal == null) {
             throw new CustomException(ExceptionCode.NOT_FOUND_USER);
         }
-
-        chatMessageService.saveMessage(roomId, request, principal);
-        messagingTemplate.convertAndSend("/sub/chatroom/" + roomId, request);
-
+        MessageQueue messageQueue = MessageQueue.of(request.getContent(), principal.getId(), roomId, LocalDateTime.now());
+        rabbitTemplate.convertAndSend(CHAT_QUEUE_NAME, messageQueue);
+        messagingTemplate.convertAndSend("/sub/chatroom/" + roomId, messageQueue);
     }
-
 }
