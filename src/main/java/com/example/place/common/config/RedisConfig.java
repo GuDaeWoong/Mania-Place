38515a23c25aa3848e058b93fa345fe72e86193d
gaeso <ghwns2239@gmail.com>
2025-08-22 21:48:10 +0900

    refactor : RedisConfig 수정

--- src/main/java/com/example/place/common/config/RedisConfig.java
+++ src/main/java/com/example/place/common/config/RedisConfig.java
@@ -15,43 +15,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-	@Bean
-	public RedissonClient redissonClient() {
-		// 환경변수에서 Serverless Redis 정보 가져오기
-		String host = System.getenv("REDIS_HOST");
-		String port = System.getenv("REDIS_PORT");
-		String username = System.getenv("REDIS_USERNAME"); // Redis 6.x 이상
-		String password = System.getenv("REDIS_PASSWORD");
-		Config config = new Config();
-		config.useSingleServer()
-			.setAddress("rediss://" + host + ":" + port) // TLS 연결
-			.setUsername(username)
-			.setPassword(password)
-			// 최소 연결 개수
-			.setConnectionMinimumIdleSize(5)
-			// 최대 연결 개수
-			.setConnectionPoolSize(20)
-			// 연결 유지시간 10초 = 10000ms
-			.setIdleConnectionTimeout(10000)
-			// Redis 서버에 연결을 시도할 때의 최대 대기 시간 10초
-			.setConnectTimeout(10000)
-			// Redis 서버로부터 응답을 기다리는 최대 시간 3초
-			.setTimeout(3000)
-			// 연결 재시도 횟수 3번
-			.setRetryAttempts(3)
-			// 연결 재시도 간의 대기시간 1.5초
-			.setRetryInterval(1500);
-		return Redisson.create(config);
-	}
+
+	// 환경변수를 한 번만 읽어서 재사용
+	private final String host = System.getenv("REDIS_HOST");
+	private final String port = System.getenv("REDIS_PORT");
+	private final String username = System.getenv("REDIS_USERNAME");
+	private final String password = System.getenv("REDIS_PASSWORD");
 
 	@Bean
 	public LettuceConnectionFactory redisConnectionFactory() {
-		// 환경변수에서 동일한 정보 가져오기
-		String host = System.getenv("REDIS_HOST");
-		String port = System.getenv("REDIS_PORT");
-		String username = System.getenv("REDIS_USERNAME");
-		String password = System.getenv("REDIS_PASSWORD");
-
 		// SSL/TLS 설정
 		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
 		redisConfig.setHostName(host);
@@ -67,6 +39,24 @@ public class RedisConfig {
 			.build();
 
 		return new LettuceConnectionFactory(redisConfig, clientConfig);
+	}
+
+	@Bean
+	public RedissonClient redissonClient() {
+		Config config = new Config();
+		config.useSingleServer()
+			.setAddress("rediss://" + host + ":" + port)
+			.setUsername(username)
+			.setPassword(password)
+			.setConnectionMinimumIdleSize(5)
+			.setConnectionPoolSize(20)
+			.setIdleConnectionTimeout(10000)
+			.setConnectTimeout(10000)
+			.setTimeout(3000)
+			.setRetryAttempts(3)
+			.setRetryInterval(1500);
+
+		return Redisson.create(config);
 	}
 
 	@Bean
